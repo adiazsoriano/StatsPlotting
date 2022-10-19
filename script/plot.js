6716b1bf630d908dc5b506658d261834813c7bf7
adiazsoriano <adiazsoriano@radford.edu>
2022-10-19 17:10:12 -0400

    updated plot.js

--- script/plot.js
+++ script/plot.js
@@ -1,1 +1,17 @@
 // main js script file
+
+
+function drawGraph() {
+
+    var trace = {
+        x: x,
+        type: 'histogram',
+        histnorm: 'probability',
+    };
+    var data = [trace];
+    Plotly.newPlot('graph',data);
+}
+
+function notifyUser() {
+    alert("Generating Graph . . .");
+}
