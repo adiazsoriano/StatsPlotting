ddec2ec47361ce54978807b8cab6aecf23ab7daf
adiazsoriano <adiazsoriano@radford.edu>
2022-10-19 10:43:11 -0400

    update page files

---
+++ php/page/databaseStats.php
@@ -0,0 +1,14 @@
+<?php 
+
+require_once "../util/calculations.php";
+
+?>
+
+<html>
+    <head>
+        <title>Database Stats</title>
+        <script src="https://cdn.plot.ly/plotly-2.14.0.min.js"></script>
+        
+    </head>
+
+</html>
