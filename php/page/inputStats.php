b8a87d4de2088f549642a32bf36a2ae4c6e3eac2
adiazsoriano <adiazsoriano@radford.edu>
2022-10-19 17:09:14 -0400

    updated inputStats.php

--- php/page/inputStats.php
+++ php/page/inputStats.php
@@ -1,24 +1,48 @@
 <?php 
 require_once "../util/calculations.php";
-
-
 ?>
 
 <html>
     <head>
         <title>Inputting Stats</title>
+        <style><?php require "../../CSS/main.css"; ?></style>
         <script src="https://cdn.plot.ly/plotly-2.14.0.min.js"></script>
-        <script> var x = <?php ?>;</script>
+        <script>
+            var x = <?php echo !empty($_POST["compute"]) ? toJSArr($_POST["numList"]) : "[]";  ?>;
+
+            function validatenum() {
+                var input = document.getElementById("input");
+                
+                if(Number.isFinite(parseFloat(input.value.toString().trim()))) {
+                    let numericInput = Number.parseInt(input.value.trim());
+                    
+                    input.value = "";
+                    input.focus();
+
+                    document.getElementById("numList").value += numericInput + " ";
+                }
+            }
+        </script>
         <script src="../../script/plot.js"></script>
     </head>
-    <body>
-        <div id="inputPlot">
-
-        </div>
-        <div class="input">
-            <form method="POST">
-                <fieldset>
+    <body <?php if(!empty($_POST["compute"])) {echo "onload='return drawGraph()'";}?>>
+        <div <?php if(!empty($_POST["compute"])) {echo "class='graphdiv' id='graph'";} ?>></div>
+        <div class="inputdiv">
+            <form method="POST" onsubmit="return notifyUser()">
+                <fieldset class="fieldsetinputdiv">
                     <legend>Enter your numbers.</legend>
+
+                    <div> 
+                        <textarea id="numList" name="numList" rows="20" cols="75" readonly></textarea>
+                        <br>
+                        <label>Enter:</label>
+                        <input id="input" type="text" placeholder="Enter numbers here.">
+                        <button id="addnum" type="button" onclick="validatenum()">Enter.</button>
+                    </div>
+
+                    <br>
+                    
+                    <button type="submit" name="compute" value="computed">Compute Graph</button>
                 </fieldset>
             </form>
         </div>
