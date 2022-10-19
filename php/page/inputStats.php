c003693f1e9eec240ce781605d682e4e8db5d23a
adiazsoriano <adiazsoriano@radford.edu>
2022-10-19 11:11:46 -0400

    updated inputStats.php

--- php/page/inputStats.php
+++ php/page/inputStats.php
@@ -8,7 +8,20 @@ require_once "../util/calculations.php";
     <head>
         <title>Inputting Stats</title>
         <script src="https://cdn.plot.ly/plotly-2.14.0.min.js"></script>
-        
+        <script> var x = <?php ?>;</script>
+        <script src="../../script/plot.js"></script>
     </head>
+    <body>
+        <div id="inputPlot">
+
+        </div>
+        <div class="input">
+            <form method="POST">
+                <fieldset>
+                    <legend>Enter your numbers.</legend>
+                </fieldset>
+            </form>
+        </div>
+    </body>
 
 </html>
