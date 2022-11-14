0065180176ce5e4f1cacf1afbc503cb38ae2566b
adiazsoriano <adiazsoriano@radford.edu>
2022-11-13 19:38:16 -0500

    login.php -> account.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                         <a class="nav-link" href="./php/page/databaseStatsPage.php">Database Data</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./php/page/login.php">Log In</a>
+                        <a class="nav-link" href="./php/page/account.php">Log In</a>
                     </li>
                 </ul>
             </div>
