a34dc2a9b9280bfffef17a2930b59550d8595cb7
jincera <jincera@radford.edu>
2022-11-16 16:48:20 -0500

    Write uploads to DB and display them

--- php/page/databaseStats.php
+++ php/page/databaseStats.php
@@ -1,13 +1,69 @@
-<?php 
-session_start();
+<?php
+// session_start();
 require_once "../util/calculations.php";
+require "../config/connection.php";
+
+
+//get num of rows from DB
+$q1 = "SELECT COUNT(*) FROM Uploads WHERE username = 'test'";
+$rCount = $pdo->query("SELECT COUNT(*) FROM Uploads WHERE username = 'test'")->fetchColumn();
+echo $rCount;
+
+//get files from db
+$i = 0;
+while ($i < $rCount) {
+    $q2 = "SELECT file FROM Uploads WHERE username = 'test'";
+    $sth = $pdo->prepare($q2);
+    $sth->execute();
+    $resArr = $sth->fetchAll(\PDO::FETCH_ASSOC);
+
+    $i++;
+}
+print_r($resArr);
+echo "<br>";
+echo "<br>";
+$str = $resArr[1]['file'];
+echo "str: " . var_dump($str);
+$file = uniqid('', true) . '.txt';
+file_put_contents('./downloads/' . $file, $str);
+$arrX = file('./downloads/' . $file, FILE_SKIP_EMPTY_LINES);
+echo "<br>";
+echo "<br>";
+var_dump($arrX);
+
+/* $query = "SELECT * FROM Uploads";
+$s1 = $pdo->query($query);
+$table = $s1->fetchAll();
+
+
+echo json_encode(unpack("I*", $table[0]["File"])) . "\n";
+echo json_encode(unpack("I*", $table[1]["File"])) . "\n"; */
+
 
 ?>
-
+<!DOCTYPE html>
 <html>
-    <head>
-        <script src="https://cdn.plot.ly/plotly-2.14.0.min.js"></script>
-        
-    </head>
+
+<head>
+    <script src="https://cdn.plot.ly/plotly-2.14.0.min.js"></script>
+    <script src="../../script/plot.js"></script>
+    <script>
+        var x = <?php echo json_encode($arrX); ?>
+        drawGraph();
+    </script>
+</head>
+
+<body <?php if (!empty($resArr)) {
+            echo "onload=' return drawGraph()'";
+        } ?>>
+    <?php if (!empty($resArr)) {
+        while ($i < $rCount) {
+            echo "<div class='graphdiv" . "$i' id='graph" . "$i'></div>";
+        }
+    } ?>
+
+    </div>
+
+</body>
 
 </html>
--- php/page/databaseStatsPage.php
+++ php/page/databaseStatsPage.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION["logintoken"])) {
+if (!isset($_SESSION["logintoken"])) {
     header("Location: account.php");
     exit();
 }
@@ -41,7 +41,7 @@ if(!isset($_SESSION["logintoken"])) {
             </div>
         </div>
     </nav>
-
+    <?php require_once "databaseStats.php" ?>
     <!-- Bootstrap scripts -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
